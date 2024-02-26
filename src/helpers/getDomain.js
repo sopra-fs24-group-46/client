fb461b658bdc511c663a5f2ff578b87e8f283f6d
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2024-02-26 18:28:17 +0100

    fix web url

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -7,7 +7,7 @@ import { isProduction } from "./isProduction"
  * @returns {string}
  */
 export const getDomain = () => {
-  const prodUrl = "sopra-fs24-serscho-server.oa.r.appspot.com" // TODO: insert your prod url for server (once deployed)
+  const prodUrl = "https://sopra-fs24-serscho-server.oa.r.appspot.com/" // TODO: insert your prod url for server (once deployed)
   // const devUrl = "https://sopra-fs24-serscho-server.oa.r.appspot.com"
   const devUrl = "http://localhost:8080"
 
