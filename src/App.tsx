054b65b95d275bd3d52479e1bc1c336b0a9771bc
daniel.amembaodhiambo <danielamemba@gmail.com>
2024-04-01 22:16:38 +0200

    map display

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
     })
       .then(response => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response shows no connection to server');
         }
         return response.json();
       })
