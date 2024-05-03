7516b325b37dbe51c7846ce2d5c6aa7845b3d923
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2024-05-03 16:34:54 +0200

    Map now displayes

--- src/components/routing/routers/GameRouter.js
+++ src/components/routing/routers/GameRouter.js
@@ -4,16 +4,25 @@ import MapBoxComponent from "../../views/MapBoxComponent";
 import {PowerUpOverlay} from "../../ui/PowerUp";
 import ComponentDev from "../../views/ComponentDev";
 import Home from "../../views/Home";
+import Question from "../../views/Question";
+
+import "styles/views/MapContainer.scss";
 
 const GameRouter = () => {
   return (
     <div >
       <PowerUpOverlay powerUp = "JOKER"/> {/* This is here as a prove that it works. It is always displayed*/}
-      <MapBoxComponent reveal={0} guessesMapReveal={[]}/>
+            <div className="map container">
+                <MapBoxComponent
+                    reveal={0}
+                    guessesMapReveal={[]}
+                />
+            </div>
       
     <Routes>
         <Route path="dev" element={<ComponentDev />} /> {/*This is accessed with localhost:3000/game/dev  */}
         <Route path="home" element={<Home />} />
+        <Route path="question" element={<Question />} />
     </Routes>
 
     </div>
