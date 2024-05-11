ac8aff5f1b18544dd376913a2d3bfdfa38620761
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2024-05-11 22:27:11 +0200

    removed audio :(

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,21 @@ import AppRouter from "./components/routing/routers/AppRouter";
  *UpdatedbyMarcoLeder
  */
 const App = () => {
-    useEffect(() => {
-        const audio = new Audio("/434061__dangerlaef__alpenhorn-kleine-scheidigg.mp3");
-        audio.loop = true;
-        audio.play()
-            .then(() => {
-                console.log("Background sound is playing.");
-            })
-            .catch(error => {
-                console.error("Failed to play background sound:", error);
-            });
-    }, [])
+  //   useEffect(() => {
+  //     const audio = new Audio(
+  //       "/434061__dangerlaef__alpenhorn-kleine-scheidigg.mp3"
+  //     );
+  //     audio.loop = true;
+  //     audio
+  //       .play()
+  //       .then(() => {
+  //         console.log("Background sound is playing.");
+  //       })
+  //       .catch((error) => {
+  //         console.error("Failed to play background sound:", error);
+  //       });
+  //   }, []);
 
-
-    return (
-
-        <AppRouter />
-
-    );
+  return <AppRouter />;
 };
 export default App;
