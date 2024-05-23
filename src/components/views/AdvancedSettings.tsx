cb6912be5404733e1795a9c2f6928ad3f3e85143
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2024-05-23 16:26:08 +0200

    rduced complexity of location types in advanced settings.

--- src/components/views/AdvancedSettings.tsx
+++ src/components/views/AdvancedSettings.tsx
@@ -307,13 +307,9 @@ const AdvancedSettings = () => {
             <div className="set-game locationTypes-container" style={{display: "flex", flexDirection: "column"}}>
               <Button onClick={() => toggleLocationTypes('LAKE')}
               className={settings.locationTypes.includes("LAKE") ? "selected" : ""}>Lakes</Button>
-              <Button onClick={() => toggleLocationTypes('ALPINE_MOUNTAIN')}
-              className={settings.locationTypes.includes("ALPINE_MOUNTAIN") ? "selected" : ""}>Alpine Mountains</Button>
-              <Button onClick={() => toggleLocationTypes('MOUNTAIN')}
+                <Button onClick={() => { toggleLocationTypes('MOUNTAIN'); toggleLocationTypes('ALPINE_MOUNTAIN'); }}
               className={settings.locationTypes.includes("MOUNTAIN") ? "selected" : ""}>Mountains</Button>
-              <Button onClick={() => toggleLocationTypes('MAIN_HILL')}
-              className={settings.locationTypes.includes("MAIN_HILL") ? "selected" : ""}>Main Hills</Button>
-              <Button onClick={() => toggleLocationTypes('HILL')}
+                <Button onClick={() => { toggleLocationTypes('HILL'); toggleLocationTypes('MAIN_HILL'); }}
               className={settings.locationTypes.includes("HILL") ? "selected" : ""}>Hills</Button>
             </div>
           </div>
