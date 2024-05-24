f46fbf28aaddff403afa4c94d8a7d4644759daf0
dhaeli <dominic.haefliger@gmx.net>
2024-05-24 08:55:09 +0200

    changes to the rules view closes #92

--- src/components/views/Rules.tsx
+++ src/components/views/Rules.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "components/ui/Button";
 import { useNavigate } from "react-router-dom";
+import { LeaderBoardPowerUp } from "components/ui/PowerUp";
 import BaseContainer from "components/ui/BaseContainer";
 import "styles/views/Game.scss";
 import "styles/views/Rules.scss";
@@ -19,17 +20,30 @@ const Rules = () => {
         <h1 className="header tiny">Game Rules!</h1>
       </div>
       <div className="rules container">
+
+        <p className="instruction-title"><strong>Here&apos;s how it works:</strong></p>
         <div className="rules content">
-          <p className="instruction-title"><strong>Here&apos;s how it works:</strong></p>
+          
 
-          <p><strong>Number of Players:</strong> The game can be played between 1 and 15 players.</p>
-          <p><strong>Number of Rounds:</strong> The game can be played for 1 to 10 rounds.</p>
-          <p><strong>Guessing Time:</strong> Each round can have a guessing time of 1 to 120 seconds.</p>
-          <p><strong>Power Ups:</strong> Power ups add temporary advantage over other players during the game. Players can choose between Joker, Shield, or Double Points during the game to try to win over their opponents.</p>
-          <p>The <strong>Joker</strong> shows a red circle where the location could be. The <strong>Double Points</strong> gives the player double the points during that round. The <strong>Shield</strong> gives the average points of all the players during the current round.</p>
+          <p><strong>Number of Players:</strong> The game can be played with up to 5 players.</p>
+          <p><strong>Number of Rounds:</strong> Each game consists of multiple rounds, which each display one location which you then have to guess on the map. You can play up to 10 rounds in a single game.</p>
+          <p><strong>Guessing Time:</strong> As soon as a location name gets shown to you and the map gets displayed, the guessing stage of the game has started. In this stage each player has to submit a guess by clicking on the map. Guessing time can be set between 2 and 120 seconds.  </p>
+          <p><strong>Power Ups:</strong> Power ups add a temporary advantage over other players during the game. Each round, before the map gets shown, every player gets a chance to choose one out of three different power ups. Each power up can only be used once per game and will last for one round.
+           Players can choose between the Joker, Double Points or the Shield power up to try to win over their opponents.
+          </p>
 
-          <Button onClick={handleGoBackClick} className="edit">Go Back</Button>
+          <div className="rules powerUp-container">
+            <div><LeaderBoardPowerUp powerUp={"JOKER"} /></div>
+            <div><LeaderBoardPowerUp powerUp={"X2"} /></div>
+            <div><LeaderBoardPowerUp powerUp={"SHIELD"} /></div>
+          </div>
+
+          <div>The <strong>Joker</strong> will help you to find the correct location. When used it will mark a region on the map with a red circle. The red circle marks the region in which the correct guess is located.
+            The <strong>Double Points</strong> power up is super easy to understand and will just double the points you score in the upcoming round. Be sure to used it when you feel confident. The <strong>Shield</strong> power up helps you when you are unsure about a location.
+            Using it will safely give you the average score of all players for the upcoming round.
+          </div>
       </div>
+      <Button onClick={handleGoBackClick} className="edit">Go Back</Button>
     </div>
   </BaseContainer>
 );
